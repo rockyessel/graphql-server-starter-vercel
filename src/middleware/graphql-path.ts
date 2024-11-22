33ffc627cfe054d4b511394ba21587b1dccfa316
Rocky Essel <rockyessel76@gmail.com>
2024-11-22 16:26:02 +0000

    update: fixed called start() with surprising state started

--- src/middleware/graphql-path.ts
+++ src/middleware/graphql-path.ts
@@ -1,16 +1,23 @@
-import { Request, Response, NextFunction } from 'express';
-import { ApolloServer } from 'apollo-server-express';
 import { IContext } from '../types';
+import { ApolloServer } from 'apollo-server-express';
+import { Request, Response, NextFunction } from 'express';
 
 export const dynamicGraphQLMiddleware = (as: ApolloServer<IContext>) => {
-  return async (req: Request, _res: Response, next: NextFunction) => {
+  return (req: Request, _res: Response, next: NextFunction) => {
     const { path } = req.params;
-    // console.log(`Dynamic path used: ${path}`);
 
-    as.applyMiddleware({
-      app: req.app as any,
-      path: `/${path}/graphql`,
-    });
+    // Ensure middleware is applied only once
+    if (!req.app.locals.appliedPaths) req.app.locals.appliedPaths = new Set();
+    const appliedPaths = req.app.locals.appliedPaths;
+
+    if (!appliedPaths.has(path)) {
+      as.applyMiddleware({
+        app: req.app as any,
+        path: `/${path}/graphql`,
+      });
+      appliedPaths.add(path);
+    }
+
     next();
   };
 };
