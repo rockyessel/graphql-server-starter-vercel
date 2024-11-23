923013421be9d9ae89d9486adf1077c40879e9b7
Rocky Essel <rockyessel76@gmail.com>
2024-11-23 01:26:46 +0000

    update: added authorization method to grapghql middleware

--- src/middleware/graphql-path.ts
+++ src/middleware/graphql-path.ts
@@ -1,21 +1,41 @@
 import { IContext } from '../types';
 import { ApolloServer } from 'apollo-server-express';
 import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { SAMPLE_AUTH_KEY } from '../libs/constants';
+import { JWT_SECRET } from '../libs/configs/env';
 
 export const dynamicGraphQLMiddleware = (as: ApolloServer<IContext>) => {
-  return (req: Request, _res: Response, next: NextFunction) => {
-    const { path } = req.params;
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { path: username } = req.params;
 
-    // Ensure middleware is applied only once
-    if (!req.app.locals.appliedPaths) req.app.locals.appliedPaths = new Set();
-    const appliedPaths = req.app.locals.appliedPaths;
+    if (username) {
+      const authHeader = req.headers[SAMPLE_AUTH_KEY] as string;
+      if (!authHeader) {
+        res.status(403).json({ error: 'Access denied: No token provided' });
+        return;
+      }
+      const token = authHeader.split(' ')[1];
+      try {
+        jwt.verify(token, JWT_SECRET);
+        // you can also verify the user's data from the decoded token
+        // if it matches to a dataset in your database.
+      } catch (error) {
+        res.status(403).json({ error: 'Access denied: Invalid token' });
+        return;
+      }
 
-    if (!appliedPaths.has(path)) {
-      as.applyMiddleware({
-        app: req.app as any,
-        path: `/${path}/graphql`,
-      });
-      appliedPaths.add(path);
+      // Apply middleware for GraphQL path dynamically
+      if (!req.app.locals.appliedPaths) req.app.locals.appliedPaths = new Set();
+      const appliedPaths = req.app.locals.appliedPaths;
+
+      if (!appliedPaths.has(username)) {
+        as.applyMiddleware({
+          app: req.app as any,
+          path: `/${username}/graphql`,
+        });
+        appliedPaths.add(username);
+      }
     }
 
     next();
